ae925c968594115a87cfe4acdfb36d3c1f0c1267
Geovana <107410963+gigixw@users.noreply.github.com>
2023-09-13 09:51:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelectorAll('.tecla');
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+//Estrutura de repetição - Enquanto 
+while () {
+
+}
