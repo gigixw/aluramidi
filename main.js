f33b4b85619e51421a3131a1742c6d608944eec3
Geovana <107410963+gigixw@users.noreply.github.com>
2023-10-06 10:54:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,24 +2,21 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-listaDeTeclas[0].onclick = tocaSom;
-
-let contador = 0;
-
 //Estrutura de repetição - Enquanto 
-while (contador < listaDeTeclas.length) {
+for (let contador = 0;contador < listaDeTeclas.length;contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = classList[1];
-
-    console.log(instrumento);
-    listaDeTeclas[contador].onclick = function () {
-          tocaSom('#som_tecla_splash');
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_ ${instrumento}`;
+    
+    tecla.onclick = function () {
+          tocaSom(idAudio);
     };
-    contador = contador + 1;
+
+    tecla.onkeydown = function () {
+    tecla.classList.add('ativa');
+    }
 
 
 }
