b067bb1f8cfcd74568921bd6ee7e796f7fa69316
Geovana <107410963+gigixw@users.noreply.github.com>
2023-11-01 09:43:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-}
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+   if (elemento && elemento.localName === 'audio') {
+       elemento.play();
+       }
+
+       else {
+       //alert('Elemento não encontrado');
+       console.log('Elemento não encontrado ou seletor inválido');
+
+       }
+    }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
@@ -9,7 +19,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_ ${instrumento}`;
+    const idAudio = `#som_${instrumento}`;
     
     tecla.onclick = function () {
           tocaSom(idAudio);
@@ -22,6 +32,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         if (evento.code === 'Space') {
 
         tecla.classList.add('ativa');
+    }
+
+    if (evento.code === 'Enter') {
+        
     }
 }
     
